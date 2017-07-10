14e1182c8788ab24d98672947d5815ab38266275
lyndonzhou <joedlen@sina.cn>
2017-07-10 15:36:38 +0800

    static_pages_spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,11 +9,18 @@ require 'rails_helper'
 #   end
 # end
 
-RSpec.describe "Static pages" do
-  describe "Home page" do
+RSpec.describe 'Static pages' do
+  describe 'Home page' do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe 'Help page' do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
     end
   end
 end
